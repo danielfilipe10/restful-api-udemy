533768ec8f91d25af1ec4988c8ada9ec850cee66
Daniel Vicente Batista <dvicentebatista@vaudoise.ch>
2024-12-27 14:19:57 +0100

    main Add BaseService and utilities

---
+++ MagicVilla_Web/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_Web.Models;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace MagicVilla_Web.Services.Interfaces
+{
+    public interface IBaseService
+    {
+        APIResponse ResponseModel { get; set; }
+
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
