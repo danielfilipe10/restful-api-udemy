65f18fcd359bb13eee6de44ef92d553135601aae
Daniel Vicente Batista <dvicentebatista@vaudoise.ch>
2025-02-04 14:37:17 +0100

    main Refactor errors

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -84,5 +84,33 @@ namespace MagicVilla_Web.Controllers
 
             return View(model);
         }
+
+        public async Task<IActionResult> DeleteVilla(int villaId)
+        {
+            var response = await _villaService.GetAsync<APIResponse>(villaId);
+
+            if (response != null && response.IsSuccess)
+            {
+                VillaDTO villaDto = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+                return View(_mapper.Map<VillaUpdateDTO>(villaDto));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteVilla(VillaDTO model)
+        {
+
+            var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(IndexVilla));
+            }
+
+            return View(model);
+        }
     }
 }
